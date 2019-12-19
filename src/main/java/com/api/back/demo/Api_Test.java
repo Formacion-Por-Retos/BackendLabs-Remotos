46ea50e0fb0f8be7c540c6bdf77c4cffc1d6bf18
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2019-12-19 16:30:01 -0500

    Error de DB.

--- src/main/java/com/api/back/demo/Api_Test.java
+++ src/main/java/com/api/back/demo/Api_Test.java
@@ -17,6 +17,12 @@ public class Api_Test {
     @Column(name = "Dato2")
     private String dato2;
 
+    @Column(name = "Dato3")
+    private String dato3;
+
+    @Column(name = "Dato4")
+    private String dato4;
+
 
     @Column(name = "Horadecreacion")
     private String creado;
@@ -25,12 +31,30 @@ public class Api_Test {
     public Api_Test(){
     }
 
-    public Api_Test(String dato, String dato2 , String creado){
+    public Api_Test(String dato, String dato2, String dato3, String dato4, String creado){
 
         setTitle(dato);
         setDato2(dato2);
+        setDato3(dato3);
+        setDato4(dato4);
         setCreado(creado);
 
+    }
+
+    public String getDato3() {
+        return dato3;
+    }
+
+    public void setDato3(String dato3) {
+        this.dato3 = dato3;
+    }
+
+    public String getDato4() {
+        return dato4;
+    }
+
+    public void setDato4(String dato4) {
+        this.dato4 = dato4;
     }
 
     public String getDato2() {
