ca6ddf52ac54fd6eba28ea3fd0011f7e8d03c3de
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-03-24 15:15:02 -0500

    Estados

--- src/main/java/com/api/back/demo/Estados.java
+++ src/main/java/com/api/back/demo/Estados.java
@@ -11,19 +11,19 @@ public class Estados {
     private int id;
 
     @Column(name = "Dato1")
-    private boolean dato1;
+    private Boolean dato1;
 
     @Column(name = "Dato2")
-    private boolean dato2;
+    private Boolean dato2;
 
     @Column(name = "Dato3")
-    private boolean dato3;
+    private Boolean dato3;
 
 
     public Estados(){
     }
 
-    public Estados(boolean dato, boolean dato2, boolean dato3){
+    public Estados(Boolean dato, Boolean dato2, Boolean dato3){
         setDato1(dato1);
         setDato2(dato1);
         setDato3(dato1);
@@ -37,27 +37,27 @@ public class Estados {
         this.id = id;
     }
 
-    public boolean isDato1() {
+    public Boolean getDato1() {
         return dato1;
     }
 
-    public void setDato1(boolean dato1) {
+    public void setDato1(Boolean dato1) {
         this.dato1 = dato1;
     }
 
-    public boolean isDato2() {
+    public Boolean getDato2() {
         return dato2;
     }
 
-    public void setDato2(boolean dato2) {
+    public void setDato2(Boolean dato2) {
         this.dato2 = dato2;
     }
 
-    public boolean isDato3() {
+    public Boolean getDato3() {
         return dato3;
     }
 
-    public void setDato3(boolean dato3) {
+    public void setDato3(Boolean dato3) {
         this.dato3 = dato3;
     }
 }
--- src/main/java/com/api/back/demo/Estados_Controller.java
+++ src/main/java/com/api/back/demo/Estados_Controller.java
@@ -9,15 +9,19 @@ public class Estados_Controller {
     @Autowired
     private Estados_Repository apiTest_repository;
 
+
+
     @PostMapping("/add")
-    public String addQuestion(@RequestParam boolean dato1, @RequestParam boolean dato2, @RequestParam boolean dato3) {
+    public String addQuestion(@RequestParam Boolean dato1, @RequestParam Boolean dato2, @RequestParam Boolean dato3) {
+
         apiTest_repository.save(new Estados(dato1,dato2,dato3));
+
           return "Se agrego. -Paul9834";
+
     }
 
     @PostMapping("/state")
-    public String modificarStatus(@RequestParam boolean dato1, @RequestParam boolean dato2, @RequestParam boolean dato3) {
-
+    public String modificarStatus(@RequestParam Boolean dato1, @RequestParam Boolean dato2, @RequestParam Boolean dato3) {
         Estados test = apiTest_repository.findById(1);
         test.setDato1(dato1);
         test.setDato2(dato2);
