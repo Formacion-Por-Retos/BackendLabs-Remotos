0150d5d77237a1b274a861c843c5559abc51654b
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-03-27 14:57:09 -0500

    Estados

--- src/main/java/com/api/back/demo/Estados_Controller.java
+++ src/main/java/com/api/back/demo/Estados_Controller.java
@@ -9,20 +9,15 @@ public class Estados_Controller {
     @Autowired
     private Estados_Repository apiTest_repository;
 
-
-
     @PostMapping("/add")
     public String addQuestion(@RequestParam Boolean dato1, @RequestParam Boolean dato2, @RequestParam Boolean dato3) {
-
         apiTest_repository.save(new Estados(dato1,dato2,dato3));
-
           return "Se agrego. -Paul9834";
 
     }
 
     @PostMapping("/state")
     public String modificarStatus(@RequestParam Boolean dato1, @RequestParam Boolean dato2, @RequestParam Boolean dato3) {
-
         Estados test = apiTest_repository.findById(1);
         test.setDato1(dato1);
         test.setDato2(dato2);
@@ -56,14 +51,9 @@ public class Estados_Controller {
     }
 
 
-
-
     @GetMapping ("/get")
     public Estados getFriendsPublication () {
         return apiTest_repository.findById(1);
     }
-
-
-
 }
 
--- src/main/java/com/api/back/demo/Estados_Repository.java
+++ src/main/java/com/api/back/demo/Estados_Repository.java
@@ -5,7 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface Estados_Repository extends JpaRepository<Estados, Integer> {
-
   Estados findById(int id);
-
 }
